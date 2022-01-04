f9c4b7c061ce1bd99572c2d069748cedc17b7c60
Mikel Muennekhoff <mikel@muennekhoff.com>
2022-01-04 21:22:52 +0100

    Add fetching

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "log"
+import (
+	"flag"
+	"fmt"
+	"github.com/sarcaustech/go-telegram-awb/pkg/awb"
+	"log"
+)
 
 func main() {
-	log.Println("AWB bot")
+
+	buildingNo := flag.Int("buildingNo", -1, "Building number")
+	streetCode := flag.Int("streetCode", -1, "Code for target street")
+	startMonth := flag.Int("startMonth", -1, "Start month of fetch period")
+	startYear := flag.Int("startYear", -1, "Start year of fetch period")
+	endMonth := flag.Int("endMonth", -1, "End month of fetch period")
+	endYear := flag.Int("endYear", -1, "End year of fetch period")
+	flag.Parse()
+
+	for index, value := range []int{*buildingNo, *streetCode, *startMonth, *startYear, *endMonth, *endYear} {
+		if value < 0 {
+			log.Println(index, value)
+			log.Fatalln("Insufficient data provided")
+		}
+	}
+
+	fetcher := awb.Fetcher{
+		BuildingNo: *buildingNo,
+		StreetCode: *streetCode,
+		StartMonth: *startMonth,
+		StartYear:  *startYear,
+		EndMonth:   *endMonth,
+		EndYear:    *endYear,
+	}
+
+	dates, err := fetcher.Fetch()
+	if err != nil {
+		log.Println(err)
+	}
+
+	for _, date := range dates {
+		log.Println(fmt.Sprintf("[%02d.%02d.%4d] %s", date.Day, date.Month, date.Year, date.Type))
+	}
 }
---
+++ pkg/awb/awb.go
@@ -0,0 +1,63 @@
+package awb
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"sort"
+)
+
+type Date struct {
+	Day   int    `json:"day"`
+	Month int    `json:"month"`
+	Year  int    `json:"year"`
+	Type  string `json:"type"`
+}
+
+type dto struct {
+	Data []Date `json:"data"`
+}
+
+type Fetcher struct {
+	BuildingNo int
+	StreetCode int
+	StartMonth int
+	StartYear  int
+	EndMonth   int
+	EndYear    int
+}
+
+func (f *Fetcher) Fetch() ([]Date, error) {
+	url := fmt.Sprintf("https://www.awbkoeln.de/api/calendar?building_number=%d&street_code=%d&start_month=%d&start_year=%d&end_month=%d&end_year=%d&form=json", f.BuildingNo, f.StreetCode, f.StartMonth, f.StartYear, f.EndMonth, f.EndYear)
+	response, err := http.Get(url)
+	if err != nil {
+		return []Date{}, err
+	}
+
+	bodyBytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		return []Date{}, err
+	}
+	defer response.Body.Close()
+
+	var dto dto
+	err = json.Unmarshal(bodyBytes, &dto)
+	if err != nil {
+		return []Date{}, err
+	}
+
+	sort.Slice(dto.Data, func(p, q int) bool {
+		pp := dto.Data[p]
+		qq := dto.Data[q]
+		if pp.Year != qq.Year {
+			return pp.Year < qq.Year
+		}
+		if pp.Month != qq.Month {
+			return pp.Month < qq.Month
+		}
+		return pp.Day < qq.Day
+	})
+
+	return dto.Data, nil
+}
